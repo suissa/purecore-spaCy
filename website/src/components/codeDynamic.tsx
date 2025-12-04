d6beec22119c14f5de33134dcb45243ff0f7befe
Jean Carlo Nascimento <jnascimento@gmail.com>
2025-12-04 07:10:19 -0300

    Convert website React code to TypeScript

---
+++ website/src/components/codeDynamic.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import dynamic from 'next/dynamic'
+
+export default dynamic(() => import('./code'), {
+    loading: () => <div style={{ color: 'white', padding: '1rem' }}>Loading...</div>,
+})
---
+++ website/src/components/markdownToReactDynamic.tsx
@@ -0,0 +1,6 @@
+// @ts-nocheck
+import dynamic from 'next/dynamic'
+
+export default dynamic(() => import('./markdownToReact'), {
+    loading: () => <p>Loading...</p>,
+})
